2a5cdcac417a6638483ca32d63ac168989acd68b
Aminu Ibrahim Abubakar <aminukano585@gmail.com>
2018-01-19 22:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function theBeatlesPlay(musicians, instruments) {
    }
    return players;
 }
+
+function johnLennonFacts(facts) {
+   for (let i = 0; i < facts.length; i++) {
+      facts[i] += "!!!";
+   }
+   return facts;
+}
